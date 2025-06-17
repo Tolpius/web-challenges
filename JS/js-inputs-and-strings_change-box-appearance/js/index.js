049f3b9c5aef4bf34ff2492d99f264384bca2e29
Julius von Bonin <juliusvonbonin@gmail.com>
2025-06-17 10:39:08 +0200

    Alle Aufgaben gelöst

--- JS/js-inputs-and-strings_change-box-appearance/js/index.js
+++ JS/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,17 @@
 console.clear();
+const colorInput = document.querySelector('[data-js="input-color"]');
+const radiusInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+colorInput.addEventListener("input", () => {
+  box.style.background = `hsl(${colorInput.value}, 70%, 60%)`;
+});
+
+radiusInput.addEventListener("input", () => {
+  box.style.borderRadius = `${radiusInput.value}%`;
+});
+
+rotationInput.addEventListener("input", () => {
+  box.style.transform = `rotate(${rotationInput.value}deg)`;
+});
--- JS/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ JS/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,9 @@
 console.clear();
+const inputElement = document.querySelector("[data-js=first-input]");
+const buttonElement = document.querySelector("[data-js=button-uppercase]");
+
+buttonElement.addEventListener("click", () => {
+  const inputValue = inputElement.value;
+  const upperCaseValue = inputValue.toUpperCase();
+  inputElement.value = upperCaseValue;
+});
