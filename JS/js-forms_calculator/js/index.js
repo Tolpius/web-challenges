5264784e5a32672836ffc0a7ca6fb4190afd0ccb
Julius von Bonin <juliusvonbonin@gmail.com>
2025-06-17 16:22:57 +0200

    Aufgaben fertig

--- JS/js-forms_calculator/js/index.js
+++ JS/js-forms_calculator/js/index.js
@@ -4,7 +4,7 @@ const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 
 function add(a, b) {
-  return a + b;
+  return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
@@ -16,15 +16,36 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  if (b === "0") {
+    return "Cannot divide by zero";
+  }
   return a / b;
 }
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData.entries(formData));
   let result;
-
+console.log(data);
   // --v-- write your code here --v--
+  console.log(data.operator);
+  switch (data.operator) {
+    case "addition":
+      result = add(data.numberA, data.numberB);
+      break;
+    case "subtraction":
+      result = subtract(data.numberA, data.numberB);
+      break;
+    case "multiplication":
+      result = multiply(data.numberA, data.numberB);
+      break;
+    case "division":
+      result = divide(data.numberA, data.numberB);
+      break;
+    default:
+      result = "Invalid operation";
+  }
 
   // --^-- write your code here --^--
 
--- JS/js-forms_checkbox-input/js/index.js
+++ JS/js-forms_checkbox-input/js/index.js
@@ -3,19 +3,34 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
-function hideTosError() {
-  tosError.setAttribute("hidden", "");
+function checkTos() {
+  if (tosCheckbox.checked) {
+    tosError.setAttribute("hidden", "");
+    return true;
+  } else {
+    tosError.removeAttribute("hidden");
+    return false;
+  }
 }
 
-function showTosError() {
-  tosError.removeAttribute("hidden");
-}
+tosCheckbox.addEventListener("change", () => {
+  checkTos();
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData.entries(formData));
   // --v-- write your code here --v--
+  console.log(data);
+  if (checkTos()) {
+    // If the terms of service are accepted, show the success message
+    successMessage.removeAttribute("hidden");
+    form.reset();
+    tosCheckbox.checked = false;
+  }
 
   // --^-- write your code here --^--
 
