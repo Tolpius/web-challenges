d5fc8e88542762cb7689cfd5a6ae1072bfa96567
Julius von Bonin <juliusvonbonin@gmail.com>
2025-06-24 16:36:11 +0200

    Aufgaben fertig

--- JS/js-structure_shapes/index.js
+++ JS/js-structure_shapes/index.js
@@ -1,23 +1,43 @@
+import { getRandomColor } from "./utils/randomColor.js"
+
+
+
 console.clear();
+
+
 
 const root = document.getElementById("root");
 
+function Circle () {
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
+  circle.style.backgroundColor = getRandomColor();
 });
+return circle;
+}
 
+
+
+function Square() {
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  square.style.backgroundColor = getRandomColor();
 });
+return square;
+}
 
+function Pentagon() {
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  pentagon.style.backgroundColor = getRandomColor();
 });
+return pentagon;
+}
 
-root.append(circle, square, pentagon);
+const circleElement = Circle();
+const squareElement = Square();
+const pentagonElement = Pentagon();
+root.append(circleElement, squareElement, pentagonElement);
--- JS/js-structure_spacerocket/js/launchSequence.js
+++ JS/js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,22 @@
 // Implement the launch sequence function here and export it as the default export.
+import { fuel } from "./core/fuel.js";
+import { getNewRocket } from "./core/rocket.js";
+import { FISHSAT, NFSAT } from "./payload/satellites.js";
+import {countdown} from "./core/countdown.js"
+import {liftoff} from "./core/liftoff.js"
+import {deployPayload} from "./core/deploy.js"
+import {loadPayload} from "./core/load.js"
+
+
+export default function launch() {
+  getNewRocket();
+  loadPayload(FISHSAT);
+  loadPayload(NFSAT);
+  fuel();
+  for (let i = 0; i < 5; i++){
+countdown();
+  }
+  liftoff();
+  deployPayload();
+
+}
